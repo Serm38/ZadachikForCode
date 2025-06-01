a5091ebcda4c794a5d3f9f122cb5ec5b669163f8
Serm38 <109290607+Serm38@users.noreply.github.com>
2025-06-01 23:43:14 +0800

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+VANTA.DOTS({
+    el: "#intro",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1f54b64,
+  color2: 0xf78361,
+  backgroundColor: 0x161a1d,
+  showLines: false
+  })
